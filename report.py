f28fffe555e1cab6965c25169c701e238cefdd9b
Ben Webb <bjwebb67@googlemail.com>
2016-06-28 12:35:59 +0100

    Add script to produce a spreadsheet report

---
+++ report.py
@@ -0,0 +1,16 @@
+import csv
+import json
+
+with open('data/data.json') as fp:
+    data_json = json.load(fp)
+
+with open('data/report.csv', 'w') as fp:
+    writer = csv.DictWriter(fp, ['publisher_name', 'converts', 'valid', 'acceptable_license'])
+    writer.writeheader()
+    for dataset in data_json:
+        writer.writerow({
+            'publisher_name': dataset['publisher']['name'],
+            'converts': bool(dataset['datagetter_metadata']['json']),
+            'valid': dataset['datagetter_metadata'].get('valid', ''),
+            'acceptable_license': dataset['datagetter_metadata']['acceptable_license'],
+        })
