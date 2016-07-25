26c6c56dba7f762fac506ea9af3a4cd51eff91bd
Ben Webb <bjwebb67@googlemail.com>
2016-07-25 14:36:48 +0100

    Output datetime downloaded in the CSV report

--- report.py
+++ report.py
@@ -5,13 +5,14 @@ with open('data/data.json') as fp:
     data_json = json.load(fp)
 
 with open('data/report.csv', 'w') as fp:
-    writer = csv.DictWriter(fp, ['publisher_name', 'title', 'file_type', 'converts', 'valid', 'acceptable_license'])
+    writer = csv.DictWriter(fp, ['publisher_name', 'title', 'datetime_downloaded', 'file_type', 'converts', 'valid', 'acceptable_license'])
     writer.writeheader()
     for dataset in data_json:
         writer.writerow({
             'publisher_name': dataset['publisher']['name'],
             'title': dataset['title'],
             'file_type': dataset['datagetter_metadata']['file_type'],
+            'datetime_downloaded': dataset['datagetter_metadata']['datetime_downloaded'],
             'converts': bool(dataset['datagetter_metadata']['json']),
             'valid': dataset['datagetter_metadata'].get('valid', ''),
             'acceptable_license': dataset['datagetter_metadata']['acceptable_license'],
