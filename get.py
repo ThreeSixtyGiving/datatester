a652baaa7a1a63a50f4c27be762ca9525ad3ed7d
Ben Webb <bjwebb67@googlemail.com>
2016-07-26 12:46:11 +0100

    Modified datetime format checker based on cove

--- get.py
+++ get.py
@@ -6,6 +6,7 @@ import tempfile
 import shutil
 import traceback
 import strict_rfc3339
+import datetime
 from jsonschema import validate, ValidationError, FormatChecker
 
 acceptable_licenses = [
@@ -23,6 +24,14 @@ unacceptable_licenses = [
 ]
 
 schema = json.loads(requests.get('https://raw.githubusercontent.com/ThreeSixtyGiving/standard/master/schema/360-giving-package-schema.json').text)
+
+
+def datetime_or_date(instance):
+    result = strict_rfc3339.validate_rfc3339(instance)
+    if result:
+        return result
+    return datetime.datetime.strptime(instance, "%Y-%m-%d")
+
 
 def convert_spreadsheet(input_path, converted_path, file_type):
     encoding = 'utf-8'
@@ -107,9 +116,13 @@ for dataset in data_json:
         os.link(json_file_name, 'data/json_acceptable_license/{}.json'.format(dataset['identifier']))
 
     if metadata['json']:
+        format_checker = FormatChecker()
+        # Use a custom format checker for datetimes, like cove does
+        # This should be removed when https://github.com/ThreeSixtyGiving/standard/pull/128 is merged
+        format_checker.checkers['date-time'] = (datetime_or_date, ValueError)
         try:
                 with open(json_file_name, 'r') as fp:
-                    validate(json.load(fp), schema, format_checker=FormatChecker())
+                    validate(json.load(fp), schema, format_checker=format_checker)
         except ValidationError:
             metadata['valid'] = False
         else:
