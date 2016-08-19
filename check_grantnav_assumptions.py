18b946cb72616b10d8e4d04592b61d1b0cfc1070
Ben Webb <bjwebb67@googlemail.com>
2016-08-19 16:14:00 +0100

    [OpenDataServices/grantnav#252] Add a stub for checking the assumptions grantnav makes

---
+++ check_grantnav_assumptions.py
@@ -0,0 +1,10 @@
+# We assmue that there are no one-to-many relationships besides location
+
+
+
+# We assume that all datasets from one publisher have the same:
+#   - accessURL
+
+
+
+# We assume that all of a funders grants are from the same publisher.
