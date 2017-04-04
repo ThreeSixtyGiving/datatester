4b4937b33c1fbf0fda9901daa49037b94c50d10d
Ben Webb <bjwebb67@googlemail.com>
2017-04-04 13:09:07 +0100

    Add some messages to run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+set -e
 rm -r data
 mkdir -p data/{original,json_all,json_valid,json_acceptable_license,json_acceptable_license_valid}
+echo 'Fetching and converting data'
 python get.py
+echo 'Generating report.csv'
 python report.py
+echo 'Creating tarball'
 tar -czf data_$(date +%F).tar.gz data
+echo 'Finished'
