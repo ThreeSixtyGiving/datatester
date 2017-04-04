e763eecacf0c2edf0beb46121b6efe1916be1ff8
Ben Webb <bjwebb67@googlemail.com>
2017-04-04 09:57:58 +0100

    Update requirements

--- update_requirements.sh
+++ update_requirements.sh
@@ -5,7 +5,7 @@ rm -rf .ve
 virtualenv --python=python3 .ve
 source .ve/bin/activate
 pip install --upgrade -r requirements.in
-pip freeze -r requirements.in > requirements.txt
+pip freeze -r requirements.in | grep -v 'pkg-resources' > requirements.txt
 # Put comments back on the same line (mostly for requires.io's benefit)
 sed -i '$!N;s/\n#\^\^/ #/;P;D' requirements*txt
 sed -i 's/^-r.*//' requirements*txt
