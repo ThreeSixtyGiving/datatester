048eae602add87dbb29a481d83a08495ef2ec45d
Ben Webb <bjwebb67@googlemail.com>
2018-02-28 16:40:00 +0000

    [ThreeSixtyGiving/test_registry#3] Add the download url to the report

--- report.py
+++ report.py
@@ -5,7 +5,7 @@ with open('data/data_all.json') as fp:
     data_json = json.load(fp)
 
 with open('data/report.csv', 'w') as fp:
-    writer = csv.DictWriter(fp, ['publisher_name', 'title', 'datetime_downloaded', 'file_type', 'downloads', 'converts', 'valid', 'acceptable_license'])
+    writer = csv.DictWriter(fp, ['publisher_name', 'title', 'downloadURL', 'datetime_downloaded', 'file_type', 'downloads', 'converts', 'valid', 'acceptable_license'])
     writer.writeheader()
     for dataset in data_json:
         if 'datagetter_metadata' not in dataset:
@@ -13,6 +13,7 @@ with open('data/report.csv', 'w') as fp:
         writer.writerow({
             'publisher_name': dataset['publisher']['name'],
             'title': dataset['title'],
+            'downloadURL': dataset['distribution'][0]['downloadURL'],
             'file_type': dataset['datagetter_metadata'].get('file_type'),
             'datetime_downloaded': dataset['datagetter_metadata']['datetime_downloaded'],
             'downloads': dataset['datagetter_metadata']['downloads'],
