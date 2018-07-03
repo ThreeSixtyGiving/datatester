bfd35b84b6e0ae84b1421701b0fe2bc7c9ee50d6
Ben Webb <ben.webb@opendataservices.coop>
2018-07-03 15:58:10 +0100

    Output every dataset in stats.json

--- stats.py
+++ stats.py
@@ -6,17 +6,16 @@ stats = []
 
 for dataset in data_all:
     json_filename = dataset['datagetter_metadata'].get('json')
-    if not json_filename:
-        continue
-    with open(json_filename) as fp:
-        aggregates = get_grants_aggregates(json.load(fp))
-    # replace sets with counts
-    for k, v in aggregates.items():
-        if isinstance(v, set):
-            aggregates[k+'_count'] = len(v)
-            del aggregates[k]
-    aggregates = {k:sorted(list(v)) if isinstance(v, set) else v for k,v in aggregates.items()}
-    dataset['datagetter_stats'] = aggregates
+    if json_filename:
+        with open(json_filename) as fp:
+            aggregates = get_grants_aggregates(json.load(fp))
+        # replace sets with counts
+        for k, v in aggregates.items():
+            if isinstance(v, set):
+                aggregates[k+'_count'] = len(v)
+                del aggregates[k]
+        aggregates = {k:sorted(list(v)) if isinstance(v, set) else v for k,v in aggregates.items()}
+        dataset['datagetter_stats'] = aggregates
     stats.append(dataset)
     with open('data/stats.json', 'w') as fp:
         json.dump(stats, fp, indent='  ', sort_keys=True)
