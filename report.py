59a6277d9048902eb7baef1a087296982cd2c3bc
Ben Webb <bjwebb67@googlemail.com>
2018-07-05 20:50:40 +0100

    Add currency information to report.csv

--- report.py
+++ report.py
@@ -1,5 +1,6 @@
 import csv
 import json
+from collections import defaultdict
 
 with open('data/stats.json') as fp:
     data_json = json.load(fp)
@@ -11,13 +12,26 @@ with open('data/report.csv', 'w') as fp:
         'converts', 'valid', 'acceptable_license', 'count', 'unique_ids_count',
         'distinct_funding_org_identifier_count',
         'distinct_recipient_org_identifier_count', 'min_award_date',
-        'max_award_date',
+        'max_award_date', 'total_amount_GBP', 'min_amount_GBP',
+        'max_amount_GBP', 'count_GBP', 'total_amount_not_GBP',
+        'min_amount_not_GBP', 'max_amount_not_GBP', 'count_not_GBP',
+        'currencies_not_GBP',
     ])
     writer.writeheader()
     for dataset in data_json:
         if 'datagetter_metadata' not in dataset:
             continue
         stats = dataset.get('datagetter_stats', {})
+
+        currencies = stats.get('currencies', {})
+        not_GBP = defaultdict(list)
+        for currency, currency_dict in currencies.items():
+            if currency == 'GBP':
+                continue
+            for field in 'min_amount', 'max_amount', 'total_amount', 'count':
+                not_GBP[field] += [str(currency_dict.get(field, ''))]
+            not_GBP['currencies'] += [currency]
+
         writer.writerow({
             'publisher_prefix': dataset['publisher']['prefix'],
             'publisher_name': dataset['publisher']['name'],
@@ -36,4 +50,13 @@ with open('data/report.csv', 'w') as fp:
             'distinct_recipient_org_identifier_count': stats.get('distinct_recipient_org_identifier_count'),
             'min_award_date': stats.get('min_award_date'),
             'max_award_date': stats.get('max_award_date'),
+            'total_amount_GBP': currencies.get('GBP', {}).get('total_amount'),
+            'min_amount_GBP': currencies.get('GBP', {}).get('min_amount'),
+            'max_amount_GBP': currencies.get('GBP', {}).get('max_amount'),
+            'count_GBP': currencies.get('GBP', {}).get('count'),
+            'total_amount_not_GBP': '; '.join(not_GBP['total_amount']),
+            'min_amount_not_GBP': '; '.join(not_GBP['min_amount']),
+            'max_amount_not_GBP': '; '.join(not_GBP['max_amount']),
+            'count_not_GBP': '; '.join(not_GBP['count']),
+            'currencies_not_GBP': '; '.join(not_GBP['currencies']),
         })
