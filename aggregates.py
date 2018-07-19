d3503c238cca4ff289f37cdc9893e9a14c38c47e
Ben Webb <ben.webb@opendataservices.coop>
2018-07-19 20:48:32 +0100

    [#8] Add list of the funders included to status.json

--- aggregates.py
+++ aggregates.py
@@ -10,10 +10,13 @@ for dataset in data_all:
         with open(json_filename) as fp:
             aggregates = get_grants_aggregates(json.load(fp))
         # replace sets with counts
-        for k, v in aggregates.items():
+        for k, v in list(aggregates.items()):
             if isinstance(v, set):
                 aggregates[k+'_count'] = len(v)
-                del aggregates[k]
+                if k == 'distinct_funding_org_identifier':
+                    aggregates[k] = sorted(list(aggregates[k]))
+                else:
+                    del aggregates[k]
         aggregates = {k:sorted(list(v)) if isinstance(v, set) else v for k,v in aggregates.items()}
         dataset['datagetter_aggregates'] = aggregates
     stats.append(dataset)
