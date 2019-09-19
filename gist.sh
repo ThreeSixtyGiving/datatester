26ad97f6c2d04c27de4fe2845582de533d890712
Michael Wood <michael.wood@opendataservices.coop>
2019-09-19 14:03:04 +0100

    gist command line option has changed now only needs gist sha

--- gist.sh
+++ gist.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Travis deploy gist
 gem install gist
-echo $GIST_TOKEN > ~/.gist  
-gist -u https://gist.github.com/30d835ae16e2a30efde8a63acf03628d data/report.csv
+echo $GIST_TOKEN > ~/.gist
+gist -u 30d835ae16e2a30efde8a63acf03628d data/report.csv
