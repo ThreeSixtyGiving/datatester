6691384b2125edc2e833384869d3d6e8dedb20b7
Ben Webb <ben.webb@opendataservices.coop>
2020-09-02 14:45:09 +0000

    requirements: Install requirements with the new pip resolver

--- update_requirements.sh
+++ update_requirements.sh
@@ -4,16 +4,17 @@
 rm -rf .ve
 python3 -m venv .ve
 source .ve/bin/activate
+pip install --upgrade pip
 if [[ "$1" == "--new-only" ]]; then
     # If --new-only is supplied then we install the current versions of
     # packages into the virtualenv, so that the only change will be any new
     # packages and their dependencies.
-    pip install -r requirements.txt
+    pip install --use-feature=2020-resolver -r requirements.txt
     dashupgrade=""
 else
     dashupgrade="--upgrade"
 fi
-pip install $dashupgrade -r requirements.in
+pip install --use-feature=2020-resolver $dashupgrade -r requirements.in
 pip freeze -r requirements.in | grep -v 'pkg-resources' > requirements.txt
 # Put comments back on the same line (mostly for requires.io's benefit)
 sed -i '$!N;s/\n#\^\^/ #/;P;D' requirements*txt
