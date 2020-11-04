a23da01a57412cab3a526ecd659b485caa561add
Bibiana Cristofol Amat <bcristofol@gmail.com>
2020-11-04 15:24:20 +0000

    gist: regular archiving of coverage.json and status.json.

--- generate-reports.sh
+++ generate-reports.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Note these python scripts all assume './data/' dir
 set -e
+echo 'Generating status.json'
 python aggregates.py
+echo 'Generating coverage.json'
 python coverage.py
 echo 'Generating report.csv'
 python report.py
--- gist.sh
+++ gist.sh
@@ -3,3 +3,5 @@
 gem install gist
 echo $GIST_TOKEN > ~/.gist
 gist -u 30d835ae16e2a30efde8a63acf03628d data/report.csv
+gist -u 0ce4a96df3c8cd773079152552ae1381 data/coverage.json
+gist -u cd8c12e426157478e2e8a537f99ecaef data/status.json
